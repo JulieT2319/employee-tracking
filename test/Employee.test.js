f8cd22db0e2ca464a322df71d23d1d1ca1129fc4
Julie T <joules2319@gmail.com>
2020-02-17 20:47:44 -0700

    added employee test

---
+++ test/Employee.test.js
@@ -0,0 +1,30 @@
+const Employee = require("../lib/Employee");
+
+test("Can instantiate Employee instance", () => {
+	const e = new Employee();
+	expect(typeof e).toBe("object");
+});
+
+test("Can set first name via constructor arguments", () => {
+	const fname = "Alice";
+	const e = new Employee(fname);
+	expect(e.first_name).toBe(fname);
+});
+
+test("Can set Last name via constructor arguments", () => {
+	const lname = "James";
+	const e = new Employee("Alice", lname);
+	expect(e.last_name).toBe(lname);
+});
+
+test("Can set role via constructor argument", () => {
+	const testValue = 3;
+	const e = new Employee("Alice", "James", testValue);
+	expect(e.role_id).toBe(testValue);
+});
+
+test("Can set manager via constructor argument", () => {
+	const testValue = "2";
+	const e = new Employee("Alice", "James", 3, testValue);
+	expect(e.manager_id).toBe(testValue);
+});
