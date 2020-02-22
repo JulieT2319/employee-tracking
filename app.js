17a7bb371836983cc2b3e17b6854ee21492c8ed1
Julie Theriault <joules2319@gmail.com>
2020-02-21 17:58:41 -0700

    added database connection.

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
 const inq = require("inquirer");
 const mysql = require("mysql");
 require("dotenv").config();
+var connection = mysql.createConnection({
+	host: "localhost",
+
+	// Your port; if not 3306
+	port: process.env.SQL_PORT,
+
+	// Your username
+	user: process.env.SQL_USER,
+
+	// Your password
+	password: process.env.SQL_PASSWORD,
+	database: process.env.SQL_DB
+});
+
+// connect to the mysql server and sql database
+connection.connect(function (err) {
+	if (err) throw err;
+});
