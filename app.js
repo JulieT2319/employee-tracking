7bb8c3f6b09de4e71fd0328b3b0960842032905c
Julie Theriault <joules2319@gmail.com>
2020-02-21 23:56:15 -0700

    display departments working

--- app.js
+++ app.js
@@ -18,22 +18,51 @@ var connection = mysql.createConnection({
 // connect to the mysql server and sql database
 connection.connect(function (err) {
 	if (err) throw err;
+	console.log(process.env.SQL_PASSWORD + "\n" + process.env.SQL_DB);
 	start();
 });
 
 function start() {
 	inq.prompt({
 		name: "addOrShow",
-		message: "Do you want search the database or add to the database?",
-		choices: ["Search", "Add", "Neither"],
+		message: "Do you want view the database or add to the database?",
+		choices: ["View", "Add", "Neither"],
 		type: "list"
 	}).then(function (choice) {
-		if (choice.addOrShow === "Search") {
-
+		if (choice.addOrShow === "View") {
+			view();
 		} else if (choice.addOrShow === "Add") {
-
+			add();
 		} else {
 			connection.end();
 		}
+	});
+}
+function view() {
+	inq.prompt({
+		name: "view",
+		message: "What do you want to view?",
+		choices: ["Departments", "Roles", "Employees"],
+		type: "list"
+	}).then(function (choice) {
+		query = "SELECT * FROM ?";
+		if (choice.view === "Departments") {
+			connection.query("SELECT * FROM department", function (err, res) {
+				console.log(res);
+				for (var i = 0; i < res.length; i++) {
+					console.log(
+						"Department " +
+						res[i].id +
+						". " +
+						res[i].name
+					);
+				};
+			});
+		} else if (choice.view === "Roles") {
+
+		} else if (choice.view === "Employees") {
+
+		}
+		start();
 	});
 }
