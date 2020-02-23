c563ba0847976311072986fae5a09d8431da0b35
Julie Theriault <joules2319@gmail.com>
2020-02-22 17:53:23 -0700

    functional add dept

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const inq = require("inquirer");
 const mysql = require("mysql");
 require("dotenv").config();
-const cTable = require('console.table');
+
+
+
 var connection = mysql.createConnection({
 	host: "localhost",
 
@@ -26,14 +28,16 @@ connection.connect(function (err) {
 function start() {
 	inq.prompt({
 		name: "addOrShow",
-		message: "Do you want view the database or add to the database?",
-		choices: ["View", "Add", "Neither"],
+		message: "What do you want to do?",
+		choices: ["View table", "Add information", "Update employee", "None of these"],
 		type: "list"
 	}).then(function (choice) {
-		if (choice.addOrShow === "View") {
+		if (choice.addOrShow === "View table") {
 			view();
-		} else if (choice.addOrShow === "Add") {
+		} else if (choice.addOrShow === "Add information") {
 			add();
+		} else if (choice.addOrShow === "Update employee") {
+			update();
 		} else {
 			connection.end();
 		}
@@ -49,6 +53,7 @@ function view() {
 		query = "SELECT * FROM ??";
 		if (choice.view === "Departments") {
 			connection.query(query, ['department'], function (err, res) {
+				console.log(res);
 				console.log("---Company Departments---");
 				console.table(res)
 				start();
@@ -71,3 +76,29 @@ function view() {
 	});
 }
 
+function add() {
+	inq.prompt({
+		name: "add",
+		message: "What do you want to add?",
+		choices: ["Department", "Role", "Employee"],
+		type: "list"
+	}).then(function (choice) {
+		query = "SELECT * FROM ??";
+		if (choice.add === "Department") {
+			inq.prompt([{
+				name: "dept",
+				message: "What is the new department?",
+				type: "input"
+			}]).then(function (data) {
+				connection.query("INSERT INTO ?? (??) VALUES (?);", ['department', 'name', data.dept]);
+				start();
+			});
+
+		} else if (choice.view === "Role") {
+
+		} else if (choice.view === "Employees") {
+
+		}
+
+	});
+}
